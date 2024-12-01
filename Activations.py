fix: add clipping to prevent gradient explosion

Activations.py
@@ -1,5 +1,4 @@
 import numpy as np
-from data import get_mnist
 
 def leakyRelu(x, alpha):
     return x if x > 0 else alpha * x
@@ -11,7 +10,7 @@ def relu_derivative(x):
     return (x > 0).astype(float)
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(np.clip(-x, -500, 500)))
     
 def sigmoid_derivative(z):
     a = sigmoid(z)